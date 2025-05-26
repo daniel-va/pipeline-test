7648e55a44d237094b0c5db833ab695ae1c61c64
Daniel von Atzigen <daniel@vatz.ch>
2025-05-26 08:15:17 +0200

    Add viewer versioning

---
+++ .github/scripts/package.config.mjs
@@ -0,0 +1,7 @@
+export const packages = {
+  api: `ghcr.io/daniel-va/pipeline-test-api`,
+  app: `ghcr.io/daniel-va/pipeline-test-app`,
+  titiler: `ghcr.io/daniel-va/pipeline-test-titiler`,
+};
+
+export const packageType = "container";
